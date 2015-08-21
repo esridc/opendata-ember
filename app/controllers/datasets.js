f0aa23930c45feb89e32e376d0417fb1c51d8790
mjuniper <mjuniper@gmail.com>
2015-08-21 13:54:21 -0600

    trying ot get pagination working

--- app/controllers/datasets.js
+++ app/controllers/datasets.js
@@ -1,4 +1,55 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+export default Ember.ArrayController.extend({
+
+  // Here, we're telling the controller that the property `page`
+  // should be "bound" to the query parameter the same name.
+  // We could map the parameter to a different property name if we wanted.
+  queryParams: [
+    'page',
+    'q'
+  ],
+
+  // defaults
+  page: 1,
+  q: null,
+  perPage: 10,
+
+  // These properties will be set by the parent route
+  totalCount: null,
+  count: null,
+
+  // The following properties will be used for the display of the pagination links
+  totalPages: function() {
+    return Math.ceil(this.get('totalCount') / this.get('perPage'));
+  }.property('page'),
+
+  prevPage: function() {
+    return this.get('page') - 1;
+  }.property('page'),
+
+  nextPage: function() {
+    return this.get('page') + 1;
+  }.property('page'),
+
+  isFirstPage: function() {
+    return this.get('page') === 1;
+  }.property('page'),
+
+  isLastPage: function() {
+    return this.get('page') >= this.get('totalPages');
+  }.property('page', 'totalPages'),
+
+  pageRange: function () {
+    var result = Ember.A();
+
+    for(var i = 1; i <= this.get('totalPages'); i++) {
+      if (i < 10) {
+        result.push(i);
+      }
+    }
+
+    return result;
+  }.property('totalPages')
+
 });
--- app/routes/datasets/index.js
+++ app/routes/datasets/index.js
@@ -2,8 +2,29 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   
-  model: function() {
-    return this.store.findQuery('dataset', {});
+  queryParams: {
+    page: {
+      refreshModel: true
+    },
+    q: {
+      refreshModel: true
+    }
+  },
+
+  model: function (params, transition) {
+    // NOTE: I think this is a bug - queryParams are available on transition but params is an empty object
+    return this.store.findQuery('dataset', transition.queryParams);
+  },
+
+  // Here, we're passing metadata to the controller
+  // This method will be executed each time the model is reloaded.
+  setupController: function(controller, model) {
+    this._super(controller, model); // Do not forget this call
+    
+    // NOTE: i don't know why we can't just call controller.___
+    var ctrl = this.controllerFor('datasets');
+    ctrl.set('totalCount', model.meta.stats.total_count);
+    ctrl.set('count', model.meta.stats.count);
   }
 
 });
