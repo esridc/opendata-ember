88f0ae0984259b13bcc56135931c8c0860e3b284
mjuniper <mjuniper@gmail.com>
2015-09-04 09:13:48 -0600

    specify mapView height

--- app/components/esri-map.js
+++ app/components/esri-map.js
@@ -24,7 +24,8 @@ export default Ember.Component.extend({
 
     var mapViewOpts = {
       container: this.elementId,  //reference to the DOM node that will contain the view
-      map: map  //references the map object created in step 3
+      map: map,  //references the map object created in step 3
+      height: 600
     };
 
     var extent, ext = dataset.get('extent');
